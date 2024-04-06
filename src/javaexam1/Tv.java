6230df2e83d389624ca3345d0630c43f109b0109
Chris <florida999@daum.net>
2024-04-06 14:41:24 +0900

    git test

--- src/javaexam1/Tv.java
+++ src/javaexam1/Tv.java
@@ -1,28 +1,28 @@
-//package javaexam1;
-//
-//public class Tv {
-////	- name:String
-////	- price:int
-////	- description:String
-////	+Tv()
-////	+Tv(name:String,price:int, 
-////	description:String)
-////	+toString():String
-//	
-//	
-//	String name;
-//	int price;
-//	String description;
-//	
-//	Tv(){
-//		
-//	}
-//	Tv (String name,int price, String description){
-//		this.name = name;
-//		this.price = price;
-//		this.description = description;
-//	}
-//	public String toString() {
-//		return this.name + " " + this.price +  " " + this.description;
-//	}
-//}
+package javaexam1;
+
+public class Tv {
+//	- name:String
+//	- price:int
+//	- description:String
+//	+Tv()
+//	+Tv(name:String,price:int, 
+//	description:String)
+//	+toString():String
+	
+	
+	String name;
+	int price;
+	String description;
+	
+	Tv(){
+		
+	}
+	Tv (String name,int price, String description){
+		this.name = name;
+		this.price = price;
+		this.description = description;
+	}
+	public String toString() {
+		return this.name + " " + this.price +  " " + this.description;
+	}
+}
